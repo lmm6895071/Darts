27d6ea8b608554b7dc8eecafe3128c4c30fff62b
lmm6895071@126.com <lmm6895071@126.com>
2021-04-15 17:17:34 +0800

    update

--- cnn/run.sh
+++ cnn/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # SBATCH -A test
-#SBATCH -J lmm
+#SBATCH -J 2nd-new
 # SBATCH -N 1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=10
 #SBATCH --gres=gpu:1
 #SBATCH -p gpu 
 #SBATCH -t 3-00:00:00
-#SBATCH -o out_scream.out
+#SBATCH -o out_2nd_new_scream.out
 
-python train_search.py --report_freq 50 
+python train_search_reverse.py --report_freq 50 --unrolled True
